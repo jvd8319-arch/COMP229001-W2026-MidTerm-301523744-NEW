b73ddfdf388bdd880c7dc169d4f72686cefa1009
JVD <jvd8319@gmail.com>
2026-02-28 04:07:31 -0500

    Renamed routers folder to routes for Render deployment

--- app/routers/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var indexController = require('../controllers/index');
-
-router.get('/', indexController.home)
-
-module.exports = router;
